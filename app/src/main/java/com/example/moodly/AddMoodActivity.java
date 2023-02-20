cc0e6d9713c01ce68685e16c94eca71ee8799876
Sandra <w19009505@northumbria.ac.uk>
2023-02-20 01:14:49 +0000

    Added functionality to nav buttons on all activities

--- app/src/main/java/com/example/moodly/AddMoodActivity.java
+++ app/src/main/java/com/example/moodly/AddMoodActivity.java
@@ -5,19 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class AddMoodActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     public BottomNavigationItemView bottomNavigationItemView;
     public FloatingActionButton floatingActionButton;
@@ -25,35 +21,37 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_mood);
 
 
-        bottomNavigationItemView = findViewById(R.id.homeButton);
+        bottomNavigationItemView =  findViewById(R.id.homeButton);
         bottomNavigationItemView = findViewById(R.id.calendarButton);
         floatingActionButton = findViewById(R.id.addButton);
-        bottomNavigationItemView = findViewById(R.id.statsButton);
-        bottomNavigationItemView = findViewById(R.id.moreButton);
+        bottomNavigationItemView =  findViewById(R.id.statsButton);
+        bottomNavigationItemView =  findViewById(R.id.moreButton);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.homeButton:
-                        Intent intentHome = new Intent(MainActivity.this, MainActivity.class);
+                        Intent intentHome = new Intent(AddMoodActivity.this , MainActivity.class);
                         startActivity(intentHome);
                         return true;
                     case R.id.calendarButton:
-                        Intent intentCalendar = new Intent(MainActivity.this, CalendarActivity.class);
+                        Intent intentCalendar = new Intent(AddMoodActivity.this , CalendarActivity.class);
                         startActivity(intentCalendar);
                         return true;
+                    case R.id.addButton:
+                        Intent intentAdd = new Intent(AddMoodActivity.this , AddMoodActivity.class);
+                        startActivity(intentAdd);
+                        return true;
                     case R.id.statsButton:
-                        Intent intentStats = new Intent(MainActivity.this, StatsActivity.class);
+                        Intent intentStats = new Intent(AddMoodActivity.this , StatsActivity.class);
                         startActivity(intentStats);
                         return true;
                     case R.id.moreButton:
-                        Intent intentMore = new Intent(MainActivity.this, MoreActivity.class);
+                        Intent intentMore = new Intent(AddMoodActivity.this , MoreActivity.class);
                         startActivity(intentMore);
                         return true;
                 }
@@ -67,22 +65,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             @Override
             public void onClick(View view) {
                 if ((FloatingActionButton)view == floatingActionButton){
-                Intent intentAdd = new Intent(MainActivity.this, AddMoodActivity.class);
-                startActivity(intentAdd);
-            }}
+                    Intent intentAdd = new Intent(AddMoodActivity.this, AddMoodActivity.class);
+                    startActivity(intentAdd);
+                }}
         });
-
     }
 
-
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
-    }}
-
-
-
-
-
-
-
-
+    }
+}
