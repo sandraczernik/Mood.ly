42d0cf02d3ef9bcd87ab9710f036b3b53be6e7cb
Sandra Czernik <w19009505@northumbria.ac.uk>
2023-04-28 10:27:02 +0100

    Added emojis to AddMoodActivity, back and next buttons

--- app/src/main/java/com/example/moodly/AddMoodActivity.java
+++ app/src/main/java/com/example/moodly/AddMoodActivity.java
@@ -13,7 +13,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class AddMoodActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+public class AddMoodActivity extends AppCompatActivity {
 
     public BottomNavigationItemView bottomNavigationItemView;
     public FloatingActionButton floatingActionButton;
@@ -24,55 +24,10 @@ public class AddMoodActivity extends AppCompatActivity implements BottomNavigati
         setContentView(R.layout.activity_add_mood);
 
 
-        bottomNavigationItemView =  findViewById(R.id.homeButton);
-        bottomNavigationItemView = findViewById(R.id.calendarButton);
-        floatingActionButton = findViewById(R.id.addButton);
-        bottomNavigationItemView =  findViewById(R.id.statsButton);
-        bottomNavigationItemView =  findViewById(R.id.moreButton);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.homeButton:
-                        Intent intentHome = new Intent(AddMoodActivity.this , MainActivity.class);
-                        startActivity(intentHome);
-                        return true;
-                    case R.id.calendarButton:
-                        Intent intentCalendar = new Intent(AddMoodActivity.this , CalendarActivity.class);
-                        startActivity(intentCalendar);
-                        return true;
-                    case R.id.addButton:
-                        Intent intentAdd = new Intent(AddMoodActivity.this , AddMoodActivity.class);
-                        startActivity(intentAdd);
-                        return true;
-                    case R.id.statsButton:
-                        Intent intentStats = new Intent(AddMoodActivity.this , StatsActivity.class);
-                        startActivity(intentStats);
-                        return true;
-                    case R.id.moreButton:
-                        Intent intentMore = new Intent(AddMoodActivity.this , MoreActivity.class);
-                        startActivity(intentMore);
-                        return true;
-                }
-                return false;
-            }
 
-        });
 
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                if ((FloatingActionButton)view == floatingActionButton){
-                    Intent intentAdd = new Intent(AddMoodActivity.this, AddMoodActivity.class);
-                    startActivity(intentAdd);
-                }}
-        });
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
-    }
+
 }
--- app/src/main/java/com/example/moodly/DBHandler.java
+++ app/src/main/java/com/example/moodly/DBHandler.java
@@ -1,16 +1,19 @@
 package com.example.moodly;
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 
+//handles all database methods
 public class DBHandler extends SQLiteOpenHelper {
     public static final String DBNAME = "Mood.ly_App";
     //creating DB handler which is used on all pages with database functions
     public DBHandler(Context context) {
         super(context, DBNAME, null, 1);
-        SQLiteDatabase MYDB = this.getWritableDatabase();
+        SQLiteDatabase MyDB = this.getWritableDatabase();
     }
 
+    // creating table to store all information added by user on AddMoodActivity.java
     @Override
     public void onCreate(SQLiteDatabase MyDB) {
         MyDB.execSQL("create Table if not exists moods(" +
@@ -22,8 +25,11 @@ public class DBHandler extends SQLiteOpenHelper {
 
         }
 
-
-
+//TODO: add new categories + display on main page
+    // insert new mood and relevant categories into the database based on user input
+    public Boolean insertNewMood;
+    SQLiteDatabase MyDB = this.getWritableDatabase();
+    ContentValues contentValues = new ContentValues();
 
 
 
@@ -32,6 +38,11 @@ public class DBHandler extends SQLiteOpenHelper {
         MyDB.execSQL("drop Table if exists moods");
 
     }
+
+
+
+
+
 
 
 }
